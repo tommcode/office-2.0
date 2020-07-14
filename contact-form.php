08fa8bc959623df25b00290ae8f0d9bb8045c138
tomson95 <tomaszgr95@interia.pl>
2020-07-14 14:51:41 +0200

    add contact form and footer

--- contact-form.php
+++ contact-form.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
+    $email_to = "tomaszgr95@interia.pl";
+    $email_subject = "Nowa wiadomość";
 
     function problem($error)
     {
