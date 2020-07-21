53c7f6b4db31a15ddff87ff252039a02dd0ea1ae
tomson95 <tomaszgr95@interia.pl>
2020-07-21 19:12:29 +0200

    add burger functionality

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const menu = document.querySelector('.hamburger'); 
-
+    const list = document.querySelector('.menu__list')
+    const navigation = document.querySelector('.page__nav')
     menu.addEventListener('click', () => { 
-    menu.classList.toggle('hamburger--active'); 
+        if (list.style.display === "block") {
+            list.style.display = "none";
+            navigation.style.transition = 'all 2s'
+            navigation.style.justifyContent = "space-between"
+          } else {
+            list.style.display = "block";
+            navigation.style.justifyContent = "start"
+          }
+    // navigation.style.display = 'block'  
+    navigation.classList.toggle('burger--menu')
+    menu.classList.toggle('hamburger--active');
+    
+    
+    
 });
 
 // ScrollReveal().reveal('.home__text', { delay: 300 });
